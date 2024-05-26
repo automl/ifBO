ef1884956362e8d6def0abd24a801980b12bd404
Neeratyoy Mallik <mallik@tf.uni-freiburg.de>
2024-05-26 22:41:33 +0200

    Adding high-level singl setup

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name="ifBO",  # Replace with your desired package name
     version="0.0.1",
-    packages=find_packages(where="ifBO"),  # Look for packages under src
+    packages=find_packages(include=["mfpbench", "neps", "pfns4hpo", "pfns_hpo"]),
 )
